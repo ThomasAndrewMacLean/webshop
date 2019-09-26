867223dc9d210d0cef76f4295729360e4a5daaf8
ThomasAndrewMacLean <thomas.maclean@marlon.be>
2019-09-27 00:45:20 +0200

    add basket button test

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,8 +1,14 @@
 <template>
   <Layout>
     <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
-  </Layout>
+        <div class="inner">
+            <span>i</span>
+            <h1>Hey</h1>
+            <p>This is an informative card that will tell you something that's... well, important!</p> <p>This is an informative card that will tell you something that's... well, important!</p> <p>This is an informative card that will tell you something that's... well, important!</p> <p>This is an informative card that will tell you something that's... well, important!</p>
+        
+    </div>  
+    
+    </Layout>
 </template>
 
 <script>
@@ -12,3 +18,41 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.inner {
+    background: rgb(9, 201, 153);
+    padding: 1em;
+    border-radius: 10px;
+    width: 250px;
+    clip-path: circle(8% at 90% 29px);
+    transition: all .5s ease-in-out;
+    cursor: pointer;
+}
+    .inner:hover {
+        clip-path: circle(75%);
+        background: #00B6FF;
+    }
+
+   .inner h1 {
+        color: white;
+        margin: 0;
+    }
+
+   .inner p {
+        color: white;
+        font-size: .8rem;
+    }
+    .inner span {
+        float: right;
+        color: white;
+        font-weight: bold;
+        transition: color .5s;
+        position: relative;
+        margin-right: 4%;
+    }
+    .inner:hover span {
+        color: rgba(255,255,255,0)
+    }
+
+</style>
